e2f2678de7ce3e4e38dfa96aee6a23836c72e7e3
Altru.dev <altrudevelop@gmail.com>
2026-09-06 08:31:38 -0700

    feat: expose radial graph API

--- radiald/__init__.py
+++ radiald/__init__.py
@@ -1,6 +1,15 @@
 """RadialD public API."""
 
 from .core import RadialExecutor, WorkResult, WorkStats
+from .graph import GraphResult, NodeTrace, RadialGraphExecutor, Stage
 
-__all__ = ["RadialExecutor", "WorkResult", "WorkStats"]
-__version__ = "0.1.0"
+__all__ = [
+    "GraphResult",
+    "NodeTrace",
+    "RadialExecutor",
+    "RadialGraphExecutor",
+    "Stage",
+    "WorkResult",
+    "WorkStats",
+]
+__version__ = "0.2.0"
