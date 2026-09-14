c49b57c34aef058014d290c890fd0260321f284e
Altru.dev <altrudevelop@gmail.com>
2026-09-13 23:21:34 -0700

    Expose assurance fabric API

--- radiald/__init__.py
+++ radiald/__init__.py
@@ -8,6 +8,21 @@ from .adapters import (
     renderdiff_signals,
     transition_closure_signals,
 )
+from .fabric import (
+    AssuranceFabricResult,
+    BindingReport,
+    EvidenceEnvelope,
+    EvidencePolicy,
+    analyze_assurance_fabric,
+    bind_evidence,
+    contradiction_signals,
+    policy_signals,
+)
+from .receipt import (
+    make_assurance_receipt,
+    seal_assurance_receipt,
+    verify_assurance_receipt,
+)
 from .canonical import CanonicalizationError, stable_digest
 from .core import RadialExecutor, WorkResult, WorkStats
 from .graph import GraphResult, NodeTrace, RadialGraphExecutor, Stage
@@ -21,7 +36,11 @@ from .weaklink import (
 )
 
 __all__ = [
+    "AssuranceFabricResult",
+    "BindingReport",
     "CanonicalizationError",
+    "EvidenceEnvelope",
+    "EvidencePolicy",
     "GraphResult",
     "NodeTrace",
     "RadialExecutor",
@@ -36,11 +55,18 @@ __all__ = [
     "WorkResult",
     "WorkStats",
     "action_receipt_signals",
+    "analyze_assurance_fabric",
     "analyze_evidence_bundle",
+    "bind_evidence",
+    "contradiction_signals",
     "dsr_result_signals",
     "physical_gate_signals",
+    "make_assurance_receipt",
+    "policy_signals",
     "renderdiff_signals",
+    "seal_assurance_receipt",
     "stable_digest",
     "transition_closure_signals",
+    "verify_assurance_receipt",
 ]
-__version__ = "0.3.0"
+__version__ = "0.4.0"
